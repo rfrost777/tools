6f9d5625a51fc2b55ce44527dc58b6e8dfd2b4e2
RFrost777 <tberrang@t-online.de>
2022-10-21 19:10:18 +0200

    Add Type hinting to improve coding style.
    Signed-off-by: RFrost777 <tberrang@t-online.de>

--- rot_encode.py
+++ rot_encode.py
@@ -10,7 +10,7 @@
 import sys
 
 
-def rot13hardcoded(phrase):
+def rot13hardcoded(phrase: str) -> str:
     # Let's hardcode a transformation scheme:
     key = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
     crypt = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
@@ -21,7 +21,7 @@ def rot13hardcoded(phrase):
     return ''.join(transform.get(char, char) for char in phrase)
 
 
-def rot13elegant(phrase):
+def rot13elegant(phrase: str) -> str:
     # Try each character in phrase and ROT13 if it is an ASCII alpha character:
     d = {}
     for character in (65, 97):
@@ -30,7 +30,7 @@ def rot13elegant(phrase):
     return ''.join([d.get(character, character) for character in phrase])
 
 
-def rot47elegant(phrase):
+def rot47elegant(phrase: str) -> str:
     d = []
     # I basically use the same trick as in rot13elegant(), check if phrase[i] is a printable
     # ASCII character, if yes then shift it, this time 47 "places"...
@@ -48,7 +48,7 @@ if len(sys.argv) != 2:
 else:
     print(f"Original string is: {sys.argv[1]}\n")
 
-    # This works both ways because ROT13/47 are mathematically an involution.
+    # This works both ways because ROT13/47 are mathematically involutions.
     print(f"* Encoded/decoded string is: {rot13hardcoded(sys.argv[1])}\n")
     print(f"* More elegant encoded: {rot13elegant(sys.argv[1])}\n\n")
     print(f"* ROT47 en-/decoded should be: {rot47elegant(sys.argv[1])}\n")
