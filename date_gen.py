a84a86f82cc888dbfe217a765aefce245f4970d4
RFrost777 <tberrang@t-online.de>
2023-04-17 21:11:55 +0200

    Minor code improvements.
    Signed-off-by: RFrost777 <tberrang@t-online.de>

--- date_gen.py
+++ date_gen.py
@@ -9,7 +9,7 @@
 
 
 def main():
-    year = 2020
+    year: int = 2020
     for month in range(10, 13):
         for day in range(1, 32):
             print(f"{year}{month:0>2}{day:0>2}")
--- rot_encode.py
+++ rot_encode.py
@@ -12,18 +12,18 @@ import sys
 
 def rot13hardcoded(phrase: str) -> str:
     # Let's hardcode a transformation scheme:
-    key = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    crypt = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
+    key: str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    crypt: str = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
     # with import string: key = string.ascii_letters; crypt = key[13:] + key[:13]
     # but let's limit our self's to pure build-in functions ;)
-    transform = dict(zip(key, crypt))
+    transform: dict = dict(zip(key, crypt))
     # Transform every character accordingly!
     return ''.join(transform.get(char, char) for char in phrase)
 
 
 def rot13elegant(phrase: str) -> str:
     # Try each character in phrase and ROT13 if it is an ASCII alpha character:
-    d = {}
+    d: dict = {}
     for character in (65, 97):
         for i in range(26):
             d[chr(i+character)] = chr((i+13) % 26 + character)
@@ -31,7 +31,7 @@ def rot13elegant(phrase: str) -> str:
 
 
 def rot47elegant(phrase: str) -> str:
-    d = []
+    d: list = []
     # I basically use the same trick as in rot13elegant(), check if phrase[i] is a printable
     # ASCII character, if yes then shift it, this time 47 "places"...
     for i in range(len(phrase)):
