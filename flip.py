f2171d0cf356155aeacaec00dfb77faffd6b1bfd
rfrost777 <tberrang@t-online.de>
2023-06-25 15:38:40 +0200

    Code cleanup and minor improvements to the code documentation.
    Signed-off-by: rfrost777 <tberrang@t-online.de>

--- flip.py
+++ flip.py
@@ -16,9 +16,8 @@ import re
 # EDIT this to point at your target address once you pressed "Start Machine"
 target_ip: str = '10.10.96.249'
 
-# Generate a key and an initialization vector for AES
-key: bytes = get_random_bytes(16)
-iv: bytes = get_random_bytes(16)
+# I copied encrypt_data() and decrypt_data() from the "leaked" sourcecode
+# in the downloadable challenge files:
 
 
 def encrypt_data(unencrypted_params: str, enc_key: bytes, i_vector: bytes) -> str:
@@ -38,38 +37,50 @@ def decrypt_data(encrypted_params: str, enc_key: bytes, i_vector: bytes) -> int:
         return 0
 
 
-# Flip one "s" in password to "r"
-user: str = 'admin&parsword=sUp3rPaSs1'
-password: str = 'sUp3rPaSs1'
-msg: str = 'logged_username=' + user +'&password=' + password
-print(msg, len(msg))
+def main():
+    # Generate a key and an initialization vector for AES
+    key: bytes = get_random_bytes(16)
+    iv: bytes = get_random_bytes(16)
 
-xor: int = ord('r') ^ ord('s')
-cipher = encrypt_data(msg, key, iv)
-# Correct the cipher on the correct position:
-cipher = cipher[:16] + hex(int(cipher[16:18], 16) ^ xor)[2:] + cipher[18:]
-# Now this should return "1":
-print(decrypt_data(cipher, key, iv))
+    # Let's do a PoC locally...
+    # Flip one "s" in password to "r"
+    user: str = 'admin&parsword=sUp3rPaSs1'
+    password: str = 'sUp3rPaSs1'
+    msg: str = 'logged_username=' + user +'&password=' + password
+    print(msg, len(msg))
 
-# Let's connect to the THM remote machine
-conn = remote(target_ip, 1337)
+    xor: int = ord('r') ^ ord('s')
+    cipher = encrypt_data(msg, key, iv)
+    # Correct the cipher on the correct position:
+    cipher = cipher[:16] + hex(int(cipher[16:18], 16) ^ xor)[2:] + cipher[18:]
+    # Now this should return "1":
+    print(decrypt_data(cipher, key, iv))
 
-print(conn.recv().decode())
-print(conn.recv().decode())
+    # Let's connect to the THM remote machine
+    # and weaponize our PoC code with pwntools:
+    conn = remote(target_ip, 1337)
 
-# Send the tampered payload:
-conn.send(b'admin&parsword=sUp3rPaSs1\r\n')
-print(conn.recv().decode())
-conn.send(b'\r\n')
+    print(conn.recv().decode())
+    print(conn.recv().decode())
 
-match = re.match(r'Leaked ciphertext: (.+)\n', conn.recv().decode())
-print('[*] Ciphertext:', match[1])
+    # Send the tampered payload:
+    conn.send(b'admin&parsword=sUp3rPaSs1\r\n')
+    print(conn.recv().decode())
+    conn.send(b'\r\n')
 
-# Correct the leaked cipher to read "admin&password=sUp3rPaSs1" again...
-cipher = match[1]
-cipher = cipher[:16] + hex(int(cipher[16:18], 16) ^ xor)[2:] + cipher[18:]
-print('[*] Modified Ciphertext:', cipher)
-conn.send(cipher.encode() + b'\r\n')
-# Hopefully collect our flag from the server, then close the connection:
-print(conn.recv().decode())
-conn.close()
+    match = re.match(r'Leaked ciphertext: (.+)\n', conn.recv().decode())
+    print('[*] Ciphertext:', match[1])
+
+    # Correct the leaked cipher to read "admin&password=sUp3rPaSs1" again...
+    cipher = match[1]
+    cipher = cipher[:16] + hex(int(cipher[16:18], 16) ^ xor)[2:] + cipher[18:]
+    print('[*] Modified Ciphertext:', cipher)
+    # ... and send it back to the server.
+    conn.send(cipher.encode() + b'\r\n')
+    # Hopefully collect our flag from the server, then close the connection:
+    print(conn.recv().decode())
+    conn.close()
+
+
+if __name__ == '__main__':
+    main()
