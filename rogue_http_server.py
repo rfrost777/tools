b2b28c79f117812ded608a60154d298251fed0ca
rfrost777 <tberrang@t-online.de>
2024-02-11 12:27:54 +0100

    Improved method overloading, fixed some names, added basic error handling.
    Signed-off-by: rfrost777 <tberrang@t-online.de>

--- rogue_http_server.py
+++ rogue_http_server.py
@@ -4,15 +4,16 @@
 # SSRF attacks and so on...
 #############################################################################
 from http.server import BaseHTTPRequestHandler, HTTPServer
-# debug:
-# from urllib.parse import unquote
+
+# setup default configuration:
+DEFAULT_PORT = 8080  # port to listen on
+DEFAULT_HOSTNAME = ''  # bind to all interfaces, for local only use '127.0.0.1'
 
 
-# On a second thought: this _might_ not be the ideal way to go about this, as I think
-# this actually overwrites HTTPRequestHandler instead of overloading its methods...
+# Maybe this is better coding practice,
 # at least it works, so lets extent http.server's RequestHandler:
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    # add support for a few basic MIME types in case we need them down the road...
+class CustomRequestHandler(BaseHTTPRequestHandler):
+    # add support for a few basic MIME content types in case we need them down the road...
     extensions_map = {
         '.manifest': 'text/cache-manifest',
         '.html': 'text/html',
@@ -24,7 +25,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         '': 'application/octet-stream',  # Default
     }
 
-    def end_headers(self):
+    def end_headers(self) -> None:
         # set headers to allow requests from any origin:
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
@@ -50,15 +51,22 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.wfile.write(response.encode('utf-8'))
 
 
+class RogueHTTPServer(HTTPServer):
+    def __init__(self, host, port):
+        server_address = (host, port)
+        HTTPServer.__init__(self, server_address, CustomRequestHandler)
+
+
 if __name__ == '__main__':
-    server_address = ('', 8080)
-    httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
-    print(f'[i] Rogue HTTP server started on port {server_address[1]}...')
+    httpd = RogueHTTPServer(DEFAULT_HOSTNAME, DEFAULT_PORT)
+    print(f'[i] Rogue HTTP server started on port {DEFAULT_PORT}...')
     try:
         # run forever (a very long time):
         httpd.serve_forever()
+    except Exception as error:
+        # add some generic error handling...
+        print(f'[e] Ooopsi, something went wrong: {error}\n')
     except KeyboardInterrupt:
-        pass
-    # maybe not that long, after all...
-    httpd.server_close()
-    print('[i] Rogue HTTP server stopped.')
+        # maybe not that long, after all...
+        httpd.server_close()
+        print('[i] Rogue HTTP server stopped. Byebye.')
