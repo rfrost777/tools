e6fe94f99425d4f501e526d314d4f5bc666423e3
rfrost777 <tberrang@t-online.de>
2024-02-21 19:04:41 +0100

    Added basic type hinting and some minor code cleanup.
    Signed-off-by: rfrost777 <tberrang@t-online.de>

--- break_rsa.py
+++ break_rsa.py
@@ -11,11 +11,15 @@ from gmpy2 import isqrt
 from math import lcm
 from Crypto.PublicKey import RSA
 import os
+from typing import Any
+
+# Path to the given, weak public RSA key:
+file_path: str = '~/id_rsa.pub'
 
 
 # Helper function to find the Greatest Common Divisor (GCD)
 # using the Euclidean Algorithm:
-def euclidean_gcd(a, b):
+def euclidean_gcd(a: Any, b: Any) -> tuple[Any, Any, Any]:
     if a == 0:
         return b, 0, 1
     g, y, x = euclidean_gcd(b % a, a)
@@ -23,7 +27,7 @@ def euclidean_gcd(a, b):
 
 
 # Think of this like a division of sorts, but for modular arithmetic ;-)...
-def modular_inverse(a, m):
+def modular_inverse(a: Any, m: Any) -> Any:
     g, x, y = euclidean_gcd(a, m)
     if g != 1:
         raise Exception('Uh-ohh! There is no modular inverse!')
@@ -33,7 +37,7 @@ def modular_inverse(a, m):
 # Python implementation of the original algorithm
 # found by Pierre de Fermat (1606-1665)
 # Time complexity should be roughly: O(sqrt(n))
-def fermat_factorize(n):
+def fermat_factorize(n: Any) -> tuple[Any, Any]:
     # First check if n is even. Since even numbers are per definitionem divisible by 2, one of the factors will
     # always be 2!
     if (n & 1) == 0:
@@ -57,14 +61,16 @@ def fermat_factorize(n):
     return a + b, a - b
 
 
-# load our given, weak public key:
-with open(os.path.expanduser('~/id_rsa.pub'), 'rb') as public_key_file:
+# Load our given, weak public key:
+key: bytes
+with open(os.path.expanduser(file_path), 'rb') as public_key_file:
     key = public_key_file.read()
-# ...and let the pycryptodom.rsa object take care of the key specifics:
+print(f'\nLoaded {file_path} for {len(key)} bytes of content!\n')
+# ...and let the pycryptodom.rsa object take care of the key implementation specifics:
 rsa_key = RSA.importKey(key)
-print('\nPublic key loaded!\n')
+
 print(f'Given e in public key: {rsa_key.e}\n')
-print(f'Size in bits is: {rsa_key.size_in_bits()}\n')
+print(f'Size (complexity) of the key in bits: {rsa_key.size_in_bits()}\n')
 
 (p, q) = (fermat_factorize(rsa_key.n))
 
