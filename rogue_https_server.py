cca685bc16527cef792ac539871762baa55a1e83
rfrost777 <tberrang@t-online.de>
2024-03-29 18:38:10 +0100

    Improve code, added use of SSLContext as required per Python 3.10+ specs.
    Signed-off-by: rfrost777 <tberrang@t-online.de>

--- rogue_https_server.py
+++ rogue_https_server.py
@@ -4,15 +4,34 @@
 # SSRF attacks and so on, now with more SSL for better OpSec :O...
 #
 # You can generate  your own SSL certificate like this:
-# openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 3650
+# openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 365
 #             -nodes -subj "/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName/CN=CommonNameOrHostname"
 #############################################################################
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 import ssl
-httpd = HTTPServer(('0.0.0.0', 8080), BaseHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket(
-    httpd.socket,
-    keyfile="key.pem",
-    certfile='cert.pem',
-    server_side=True)
-httpd.serve_forever()
+
+# default configuration:
+DEFAULT_PORT: int = 4443
+
+if __name__ == '__main__':
+    httpd = HTTPServer(('0.0.0.0', DEFAULT_PORT), SimpleHTTPRequestHandler)
+    # get the SSL socket wrapper ready, :
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    ssl_context.check_hostname = False  # Or else only the hostname that matches the certificate will be accepted!
+    ssl_context.load_cert_chain(
+        certfile='/home/tberrang/cert.pem',
+        keyfile="/home/tberrang/key.pem"
+    )
+    httpd.socket = ssl_context.wrap_socket(httpd.socket, server_side=True)
+
+    print(f'[i] Rogue HTTPS server started on port: {DEFAULT_PORT}...')
+    try:
+        # run forever (a very long time!):
+        httpd.serve_forever()
+    except Exception as error:
+        # pretend to add some generic error handling ;..;
+        print(f'[e] Ewwww, something went wrong: {error}\n')
+    except KeyboardInterrupt:
+        # ... maybe not _that_ long, after all...
+        httpd.server_close()
+        print('[i] Rogue HTTPS server stopped. Byebye!')
