6a36951a981954835521d94e5e6b0e9f287c6908
rfrost777 <tberrang@t-online.de>
2024-03-29 17:55:37 +0100

    Copypasta banner text and improve fugly formating...
    Signed-off-by: rfrost777 <tberrang@t-online.de>

--- rogue_https_server.py
+++ rogue_https_server.py
@@ -1,7 +1,13 @@
-#!/bin/python3
-# You can generate  your own certificate like this:
-# openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 3650 -nodes -subj "/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName/CN=CommonNameOrHostname"
-from http.server import HTTPServer, BaseHTTPRequestHandler 
+#!/usr/bin/env python3
+#############################################################################
+# Extended http.server module to capture post requests from indirect (blind)
+# SSRF attacks and so on, now with more SSL for better OpSec :O...
+#
+# You can generate  your own SSL certificate like this:
+# openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 3650
+#             -nodes -subj "/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName/CN=CommonNameOrHostname"
+#############################################################################
+from http.server import HTTPServer, BaseHTTPRequestHandler
 import ssl
 httpd = HTTPServer(('0.0.0.0', 8080), BaseHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket(
