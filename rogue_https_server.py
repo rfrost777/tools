81f585b54e0d0b40ba2273ccd94ec5f938023b5e
rfrost777 <tberrang@t-online.de>
2024-04-02 19:25:22 +0200

    Use /tmp as default key storage!
    Signed-off-by: rfrost777 <tberrang@t-online.de>

--- rogue_https_server.py
+++ rogue_https_server.py
@@ -4,7 +4,7 @@
 # SSRF attacks and so on, now with more SSL for better OpSec :O...
 #
 # You can generate  your own SSL certificate like this:
-# openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 365
+# openssl req -x509 -newkey rsa:4096 -keyout /tmp/key.pem -out /tmp/cert.pem -sha256 -days 365
 #             -nodes -subj "/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName/CN=CommonNameOrHostname"
 #############################################################################
 from http.server import HTTPServer, SimpleHTTPRequestHandler
@@ -12,6 +12,10 @@ import ssl
 
 # default configuration:
 DEFAULT_PORT: int = 4443
+# Someone suggested to use /tmp as storage.
+# Makes kinda sense for throw-away certificates I guess, so there you go!
+PRIVATE_KEY_FILE: str = "/tmp/key.pem"
+CERTIFICATE_FILE: str = "/tmp/cert.pem"
 
 if __name__ == '__main__':
     httpd = HTTPServer(('0.0.0.0', DEFAULT_PORT), SimpleHTTPRequestHandler)
@@ -20,8 +24,8 @@ if __name__ == '__main__':
     ssl_context.check_hostname = False  # Or else only the hostname that matches the certificate will be accepted!
     # load our throw-away certificate and private keyfile we generated above:
     ssl_context.load_cert_chain(
-        certfile='cert.pem',
-        keyfile='key.pem'
+        certfile=CERTIFICATE_FILE,
+        keyfile=PRIVATE_KEY_FILE
     )
     httpd.socket = ssl_context.wrap_socket(httpd.socket, server_side=True)
 
